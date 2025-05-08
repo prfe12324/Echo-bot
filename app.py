06e001e6d3894c350881e1729484fb15f70dc437
prfe12324 <0110509@gmail.com>
2025-05-08 11:45:13 +0800

    init

--- app.py
+++ app.py
@@ -27,6 +27,7 @@ from linebot.v3.messaging import (
     QuickReply,
     QuickReplyItem
 )
+
 from linebot.v3.webhooks import (
     MessageEvent,
     TextMessageContent,
@@ -39,7 +40,7 @@ import os
 
 app = Flask(__name__)
 configuration = Configuration(access_token=os.getenv('CHANNEL_ACCESS_TOKEN'))
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+line_handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -47,13 +48,13 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     try:
-        handler.handle(body, signature)
+        line_handler.handle(body, signature)
     except InvalidSignatureError:
         app.logger.info("Invalid signature.")
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessageContent)
+@line_handler.add(MessageEvent, message=TextMessageContent)
 def handle_text_message(event):
     text = event.message.text
     with ApiClient(configuration) as api_client:
